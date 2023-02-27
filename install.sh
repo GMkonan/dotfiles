6708feec2bb21ad9dffb5847b6c1f829e83cc2e6
GMkonan <guilhemont@hotmail.com>
2023-02-27 08:00:47 -0300

    :wrench:

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ ln -nsf "$PWD"/configs/.zshrc ~/.zshrc
 #install zsh spaceship theme
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
 
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
 
 zsh
 source ~/.zshrc
