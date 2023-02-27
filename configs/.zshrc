f032b75bad7901ad8975550dcdd9240b68eab8c5
GMkonan <guilhemont@hotmail.com>
2023-02-27 07:16:17 -0300

    Add install docker script

--- configs/.zshrc
+++ configs/.zshrc
@@ -1,5 +1,5 @@
 # Path to your oh-my-zsh installation.
-export ZSH="/home/gmkonan/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 export ZSH_CUSTOM=$ZSH/custom
 
 ZSH_THEME="spaceship"
--- install.sh
+++ install.sh
@@ -43,7 +43,23 @@ source ~/.zshrc
 
 echo "Installing Docker..."
 #https://matt-wxw.medium.com/one-command-to-install-docker-and-docker-compose-on-ubuntu-febb8bc5cb72
-/bin/bash -c "$(curl -fsSL https://git.io/JDGfm)"
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update && apt-get upgrade -y
+apt-get install -y \
+    ca-certificates \
+    gnupg \
+    lsb-release
+rm -rf /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update && apt-get upgrade -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
+usermod -a -G docker $USER
+curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 
 # symbolic links, f flag can overwrite files
 ln -nsf "$PWD"/configs/.gitconfig ~/.gitconfig
