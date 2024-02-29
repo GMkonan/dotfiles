fbca968dcbf8cf762bacdf924761b75f615caaa9
GMkonan <guilhemont@hotmail.com>
2024-02-29 18:17:52 -0300

    Add lines to config files instead of creating new ones

--- devcontainer/install.sh
+++ devcontainer/install.sh
@@ -1,3 +1,6 @@
 ### https://medium.com/@jamiekt/vscode-devcontainer-with-zsh-oh-my-zsh-and-agnoster-theme-8adf884ad9f6
-cp ./.bashrc ~
-cp ./gitconfig ~
+# cp ./.bashrc ~
+# cp ./gitconfig ~
+
+cat bashrc.additions >> ~/.bashrc
+cat gitconfig.additions >> ~/.gitconfig
