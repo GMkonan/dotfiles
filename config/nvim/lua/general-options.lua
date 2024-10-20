b804ad3f2ec42cd73606efeeab55145b9514c5c0
GMkonan <guilhemont@hotmail.com>
2024-10-19 22:41:34 -0300

    cleanup some old nvim files

--- config/nvim/lua/general-options.lua
+++ config/nvim/lua/general-options.lua
@@ -29,7 +29,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-    vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
@@ -50,7 +50,7 @@ vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 350
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -5,3 +5,22 @@ return {
   -- use opts = {} for passing setup options
   -- this is equivalent to setup({}) function
 }
+
+-- example config?
+--
+-- autopairs
+-- https://github.com/windwp/nvim-autopairs
+
+-- return {
+--   'windwp/nvim-autopairs',
+--   event = 'InsertEnter',
+--   -- Optional dependency
+--   dependencies = { 'hrsh7th/nvim-cmp' },
+--   config = function()
+--     require('nvim-autopairs').setup {}
+--     -- If you want to automatically add `(` after selecting a function or method
+--     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+--     local cmp = require 'cmp'
+--     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+--   end,
+-- }
