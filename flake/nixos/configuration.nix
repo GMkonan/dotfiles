e049f1c6bfabd3963653e183d0d2beac268f9341
GMkonan <guilhemont@hotmail.com>
2024-11-18 21:25:01 -0300

    disable xserver x11

--- flake/nixos/configuration.nix
+++ flake/nixos/configuration.nix
@@ -34,17 +34,17 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
