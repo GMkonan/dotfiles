da1123c3393047d6dcda769501a5f8a51ef4a239
GMkonan <guilhemont@hotmail.com>
2024-11-18 21:24:44 -0300

    Add waybar and simple hyprland config

--- flake/home-manager/modules/wms/hyprland.nix
+++ flake/home-manager/modules/wms/hyprland.nix
@@ -2,5 +2,26 @@
   wayland.windowManager.hyprland = {
     enable = true;
     # xwayland.enable = true;
+    
+    settings = {
+      "$mod" = "SUPER";
+    bind =
+      [
+        "$mod, F, exec, firefox"
+        ", Print, exec, grimblast copy area"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+            let ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9)
+      );
+    };
   };
 }
