84356b27e2daac5056b7fb11e2833f6f8889f05e
GMkonan <guilhemont@hotmail.com>
2024-11-23 16:00:09 -0300

    bootloader grub works

--- flake/nixos/modules/bootloader.nix
+++ flake/nixos/modules/bootloader.nix
@@ -2,5 +2,6 @@
 # boot.loader.systemd-boot.enable = true;
 boot.loader.grub.enable = true;
 boot.loader.grub.devices = [ "nodev" ];
+boot.loader.grub.efiSupport = true;
 boot.loader.efi.canTouchEfiVariables = true;
 }
