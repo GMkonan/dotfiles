f230b95d6a9a539ab1176c743a7fe610e0914c7a
GMkonan <guilhemont@hotmail.com>
2025-01-24 13:53:56 -0300

    Add githelpers file with pretty git log func

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 ln -Fs ~/code/projects/dotfiles/.zshrc ~/.zshrc
 ln -Fs ~/code/projects/dotfiles/.aliases ~/.aliases
 ln -Fs ~/code/projects/dotfiles/.gitconfig ~/.gitconfig
+ln -Fs ~/code/projects/dotfiles/.githelpers ~/.githelpers
 ln -Fs ~/code/projects/dotfiles/.tmux.conf ~/.tmux.conf
 ln -Fs ~/code/projects/dotfiles/config/kitty ~/.config/kitty
 ln -Fs ~/code/projects/dotfiles/config/nvim ~/.config/nvim
-ln -Fs ~/code/projects/dotfiles/.p10k.zsh ~/.p10k.zsh
 
