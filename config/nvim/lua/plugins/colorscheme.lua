8751dd15bba7fe99e27e1f67205429191e107ae2
GMkonan <guilhemont@hotmail.com>
2025-02-16 21:05:42 -0300

    Activate transparent background in colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -10,6 +10,9 @@ return {
   name = 'catppuccin',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
+    require('catppuccin').setup {
+      transparent_background = true,
+    }
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
