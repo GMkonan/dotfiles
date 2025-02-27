d053eac59c84496705cccb72f500747ca8c04f1c
GMkonan <guilhemont@hotmail.com>
2025-02-27 15:45:52 -0300

    divide mac and linux install aka symlink

--- install_linux.sh
+++ install_linux.sh
@@ -1,10 +1,9 @@
 # symlinks
 # F flag to force creation even if file exists
-#
+
 ln -Fs $PWD/.zshrc $HOME/.zshrc
 ln -Fs $PWD/.gitconfig $HOME/.gitconfig
 ln -Fs $PWD/.githelpers $HOME/.githelpers
 ln -Fs $PWD/.tmux.conf $HOME/.tmux.conf
-ln -Fs $PWD/.aerospace.toml $HOME/.aerospace.toml
 ln -Fs $PWD/config/nvim $HOME/.config/nvim
 ln -Fs $PWD/config/ghostty $HOME/.config/ghostty
