de798917fe226d0924bd694d1e32267f18126276
GMkonan <guilhemont@hotmail.com>
2025-03-16 00:50:31 -0300

    Remove go and cargo paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,13 +1,13 @@
 # All paths
-
+#
 # Homebrew
 export PATH="/opt/homebrew/bin:$PATH"
 
 # go and go vm (via g install)
-export GOPATH="$HOME/go"; export GOROOT="$HOME/.go"; export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
-alias ggovm="$GOPATH/bin/g";
+# export GOPATH="$HOME/go"; export GOROOT="$HOME/.go"; export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
+# alias ggovm="$GOPATH/bin/g";
 
-source "$HOME/.cargo/env"
+# source "$HOME/.cargo/env"
 
 # bun
 export BUN_INSTALL="$HOME/.bun"
