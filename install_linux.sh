49d568f1b03c2081f3e8e94731ec4c871b8b8300
GMkonan <guilhemont@hotmail.com>
2025-05-30 16:13:53 -0300

    Add psqlrc options

--- install_linux.sh
+++ install_linux.sh
@@ -5,5 +5,6 @@ ln -Fs $PWD/.zshrc $HOME/.zshrc
 ln -Fs $PWD/.gitconfig $HOME/.gitconfig
 ln -Fs $PWD/.githelpers $HOME/.githelpers
 ln -Fs $PWD/.tmux.conf $HOME/.tmux.conf
+ln -Fs $PWD/.psqlrc $HOME/.psqlrc
 ln -Fs $PWD/config/nvim $HOME/.config/nvim
 ln -Fs $PWD/config/ghostty $HOME/.config/ghostty
