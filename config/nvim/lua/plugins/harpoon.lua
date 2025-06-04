1846464934a63a3eb7779f3477f1bef26fc4b396
GMkonan <guilhemont@hotmail.com>
2025-06-03 23:49:59 -0300

    Add descs to harpoon keybinds

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -5,14 +5,15 @@ return {
   config = function()
     local harpoon = require("harpoon")
     harpoon.setup({})
-    vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end, { desc = 'Add to [H]arpoon list' })
+    vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+      { desc = "Open harpoon quick menu" })
 
-    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-    vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = 'Go to buffer 1' })
+    vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = 'Go to buffer 2' })
+    vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = 'Go to buffer 3' })
+    vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = 'Go to buffer 4' })
+    vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = 'Go to buffer 5' })
 
     -- Toggle previous & next buffers stored within Harpoon list
     -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
