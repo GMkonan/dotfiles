6e3f72f1db8fd469b0070efb5bc740203859d640
GMkonan <guilhemont@hotmail.com>
2025-08-11 22:25:02 -0300

    comments on surround plugin neovim

--- config/nvim/lua/plugins/surround.lua
+++ config/nvim/lua/plugins/surround.lua
@@ -1,3 +1,4 @@
+-- https://github.com/kylechui/nvim-surround
 return {
   'kylechui/nvim-surround',
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
@@ -5,6 +6,9 @@ return {
   config = function()
     require('nvim-surround').setup {
       -- Configuration here, or leave empty to use defaults
+      -- cs ([C]hange [S]urrounding + existing_surrounding + new_surrounding)
+      -- ds ([D]elete [S]urrounding + existing_surrounding)
+      -- etc
     }
   end,
 }
